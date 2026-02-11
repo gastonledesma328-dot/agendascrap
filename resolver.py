c01ae0c66234d9814925af8cca7c7f792394e938
gastonledesma328-dot <gastonledesma328@gmail.com>
2026-02-11 16:18:59 -0300

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,10 +1,16 @@
 import requests
+import unicodedata
 
 CANALES_URL = "https://raw.githubusercontent.com/gastonledesma328-dot/agendascrap/refs/heads/main/canales%20(8).json"
 
 
-def normalizar(texto):
-    return texto.lower().replace("+", "").replace(" ", "")
+def normalizar_texto(texto):
+    texto = texto.lower()
+    texto = unicodedata.normalize("NFD", texto)
+    texto = texto.encode("ascii", "ignore").decode("utf-8")
+    texto = texto.replace("+", "")
+    texto = texto.replace(" ", "")
+    return texto
 
 
 def cargar_canales():
@@ -16,8 +22,10 @@ def cargar_canales():
 def buscar_canal_propio(nombre_detectado):
     canales = cargar_canales()
 
+    nombre_detectado = normalizar_texto(nombre_detectado)
+
     for canal in canales:
-        if normalizar(nombre_detectado) in normalizar(canal["name"]):
+        if nombre_detectado in normalizar_texto(canal["name"]):
             return canal["streamUrl"]
 
     return None
