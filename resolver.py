ff4b2e6a6a3bca9a0d9181a0a2b142d30dae92af
gastonledesma328-dot <gastonledesma328@gmail.com>
2026-02-11 15:38:37 -0300

    Create resolver.py

---
+++ resolver.py
@@ -0,0 +1,23 @@
+import requests
+
+CANALES_URL = "https://raw.githubusercontent.com/gastonledesma328-dot/agendascrap/refs/heads/main/canales%20(8).json"
+
+
+def normalizar(texto):
+    return texto.lower().replace("+", "").replace(" ", "")
+
+
+def cargar_canales():
+    r = requests.get(CANALES_URL)
+    r.raise_for_status()
+    return r.json()
+
+
+def buscar_canal_propio(nombre_detectado):
+    canales = cargar_canales()
+
+    for canal in canales:
+        if normalizar(nombre_detectado) in normalizar(canal["name"]):
+            return canal["streamUrl"]
+
+    return None
